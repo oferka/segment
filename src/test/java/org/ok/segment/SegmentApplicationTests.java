119174dc5f4b69cc589a81ec8cd9cba6877c7d2a
ofer.karp <ofer.karp@gmail.com>
2021-11-25 15:12:32 +0200

    Initial commit

---
+++ src/test/java/org/ok/segment/SegmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ok.segment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
