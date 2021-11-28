41880178f52b0d8e9acb9a72b82b3bb0e965ab0e
ofer.karp <ofer.karp@gmail.com>
2021-11-28 17:19:26 +0200

    log levels config

--- src/main/java/org/ok/segment/model/Segment.java
+++ src/main/java/org/ok/segment/model/Segment.java
@@ -6,10 +6,13 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import static org.springframework.data.elasticsearch.annotations.FieldType.Keyword;
 
 @ToString
 @AllArgsConstructor
@@ -22,11 +25,13 @@ public class Segment {
     @NotNull
     @Size(min = 2, max = 64)
     @NotBlank
+    @Field(type = Keyword)
     private final String id;
 
     @Getter
     @NotNull
     @Size(min = 2, max = 64)
     @NotBlank
+    @Field(type = Keyword)
     private final String name;
 }
